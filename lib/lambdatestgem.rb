92417c9646c9af302b6f58502c0c173a39b1b961
Maximilian Haack <max@haack.xyz>
2019-03-22 16:56:45 +0700

    Initial commit

---
+++ lib/lambdatestgem.rb
@@ -0,0 +1,6 @@
+require "lambdatestgem/version"
+
+module Lambdatestgem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lambdatestgem/version.rb
@@ -0,0 +1,3 @@
+module Lambdatestgem
+  VERSION = "0.1.0"
+end
